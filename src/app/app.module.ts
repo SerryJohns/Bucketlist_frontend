ba15ed4662283103afa929e6896c0b7ca30017e5
Seremba John <john.seremba@andela.com>
2017-07-28 14:54:28 +0300

    Add account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserModule,
+    FormsModule
+      ],
+  providers: [ ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
