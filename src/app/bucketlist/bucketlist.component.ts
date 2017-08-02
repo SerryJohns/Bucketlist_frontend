6caf917775547c5b10040b47378de8e000462ebf
Seremba John <john.seremba@andela.com>
2017-08-02 15:16:42 +0300

    Add logout functionality

--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './../services/auth/auth.service';
+
 @Component({
   selector: 'app-bucketlist',
   templateUrl: './bucketlist.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BucketlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
   
   public isAuthenticated(): boolean {
     return this.isValidToken();
@@ -14,6 +15,12 @@ export class AuthService {
     let token: string;
     token = localStorage.getItem('id_token');
     return token && tokenNotExpired ? true: false;
+  }
+
+  public logout(): void {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('currentUser');
+    this.router.navigate(['login']);
   }
 
 }
