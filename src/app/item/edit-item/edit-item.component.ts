6a3ba0f08d883d76785deaa5175a6232712c5f23
Seremba John <john.seremba@andela.com>
2017-08-17 09:21:33 +0300

    Initialize edit item component

--- src/app/item/edit-item/edit-item.component.ts
+++ src/app/item/edit-item/edit-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from "../item";
 
 @Component({
   selector: 'app-edit-item',
@@ -9,7 +10,14 @@ export class EditItemComponent implements OnInit {
 
   constructor() { }
 
+  @Input() item: Item;
+  private model: any = { };
+  
   ngOnInit() {
+    this.model = this.item;
   }
+  
+  private updateItem(): void {
 
+  }
 }
